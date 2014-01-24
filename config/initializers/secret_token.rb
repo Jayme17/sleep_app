46fcda79742c9e466b72978d1581551984e102fb
jayme17 <jjones@chicagotechacademy.org>
2014-01-24 12:51:08 -0600

    First Set Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SleepApp::Application.config.secret_key_base = '2350898a663b18de49be47f2ffff2293b61c74c624d95f7f311ed2fcd688397b428b8f3722b041898c7e760cf51223d56e4d94309a09b19fa1b81e3a5c2d7b49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SleepApp::Application.config.session_store :cookie_store, key: '_sleep_app_session'
